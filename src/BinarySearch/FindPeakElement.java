63b1b334d3944fbefe1fc0a9f8626bb03a269a0f
Adam Wang <hwang12wou@gmail.com>
2017-06-14 13:36:37 -0400

    real

--- src/BinarySearch/FindPeakElement.java
+++ src/BinarySearch/FindPeakElement.java
@@ -12,28 +12,30 @@ package BinarySearch;
  * Return index 1 (which is number 2) or 6 (which is number 7)
  */
 //https://siddontang.gitbooks.io/leetcode-solution/content/array/find_peak_element.html
+
+// 只要 mid左 > mid 就有戏
 public class FindPeakElement {
 	public int findPeak(int[] A) {
 		// write your code here
-		int n = A.length;
-		if (n == 1) {
-			return 0;
-		}
+		 if(A == null || A.length == 0){
+	            return -1;
+	        }
+			int n = A.length;
 
-		int start = 0;
-		int end = n - 1;
-		int mid = 0;
+			int start = 0;
+			int end = n - 1;
+			int mid = 0;
 
-		while (start <= end) {
-			mid = start + (end - start) / 2;
-			if ((mid == 0 || A[mid] >= A[mid - 1]) && (mid == n - 1 || A[mid] >= A[mid + 1])) {
-				return mid;
-			} else if (mid > 0 && A[mid - 1] > A[mid]) {
-				end = mid - 1;
-			} else {
-				start = mid + 1;
+			while (start <= end) {
+				mid = start + (end - start) / 2;
+				if ((mid == 0 || A[mid] >= A[mid - 1]) && (mid == n - 1 || A[mid] >= A[mid + 1])) {
+					return mid;
+				} else if (mid > 0 && A[mid - 1] > A[mid]) {
+					end = mid - 1;
+				} else {
+					start = mid + 1;
+				}
 			}
-		}
-		return mid;
+			return mid;
 	}
 }
