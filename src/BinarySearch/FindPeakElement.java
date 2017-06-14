f6e4ded0bedae40f6ea2a3d9ff082e4ca5253be0
Adam Wang <hwang12wou@gmail.com>
2017-06-14 13:29:11 -0400

    gitPage sol

--- src/BinarySearch/FindPeakElement.java
+++ src/BinarySearch/FindPeakElement.java
@@ -11,40 +11,29 @@ package BinarySearch;
  * Given [1, 2, 1, 3, 4, 5, 7, 6]
  * Return index 1 (which is number 2) or 6 (which is number 7)
  */
+//https://siddontang.gitbooks.io/leetcode-solution/content/array/find_peak_element.html
 public class FindPeakElement {
 	public int findPeak(int[] A) {
 		// write your code here
-		if (A == null || A.length <= 2) {
-			return -1;
+		int n = A.length;
+		if (n == 1) {
+			return 0;
 		}
-		int left = 0;
-		int right = A.length - 1;
-		int mid = left + (right - left) / 2;
-		if (A[mid] > A[mid - 1] && A[mid] > A[mid + 1]) {
-			return mid;
-		}
-		int leftVal = binarySearch(A, left, mid);
-		int rightVal = binarySearch(A, mid, right);
-		if (leftVal != -1) {
-			return leftVal;
-		} else if (rightVal != -1) {
-			return rightVal;
-		} else {
-			return -1;
-		}
-	}
 
-	private int binarySearch(int[] A, int left, int right) {
-		while (left < right) {
-			int mid = left + (right - left) / 2;
-			if (A[mid] > A[mid - 1] && A[mid] > A[mid + 1]) {
+		int start = 0;
+		int end = n - 1;
+		int mid = 0;
+
+		while (start <= end) {
+			mid = start + (end - start) / 2;
+			if ((mid == 0 || A[mid] >= A[mid - 1]) && (mid == n - 1 || A[mid] >= A[mid + 1])) {
 				return mid;
-			} else if (A[mid] > A[mid - 1] && A[mid] < A[mid + 1]) {
-				left = mid + 1;
-			} else if (A[mid] < A[mid - 1] && A[mid] > A[mid + 1]) {
-				right = mid - 1;
+			} else if (mid > 0 && A[mid - 1] > A[mid]) {
+				end = mid - 1;
+			} else {
+				start = mid + 1;
 			}
 		}
-		return -1;
+		return mid;
 	}
 }
