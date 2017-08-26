bb7b9641ddee23f04277bc71600b718b6f050164
Adam Wang <hwang12wou@gmail.com>
2017-08-26 17:17:29 -0400

    validated

--- src/main/java/QuickSort.java
+++ src/main/java/QuickSort.java
@@ -8,46 +8,39 @@ public class QuickSort {
 		if(array == null || array.length <= 1){
 			return;
 		}
-		QuickSort(array,0,array.length - 1);
+		quickSort(array, 0, array.length - 1);
 	}
 
-	private void QuickSort(int[] array, int left, int right) {
+	private void quickSort(int[] array, int left, int right){
 		if(left >= right){
 			return;
 		}
-		int pivotPosAfterSort = sortArray(array,left,right);
-		//Dont move pivot
-		QuickSort(array,left,pivotPosAfterSort - 1);
-		QuickSort(array,pivotPosAfterSort + 1,right);
+		int pivotPos = sortArray(array, left, right);
+		quickSort(array, left, pivotPos);
+		quickSort(array, pivotPos + 1, right);
 	}
 
-	private int sortArray(int[] array, int left, int right) {
-		int randomPivotPos = randomPivot(left,right);
-		int endPos = right;
-		int pivot = array[randomPivotPos];
-		swap(array,randomPivotPos,right);
-		//right start position left shift an space for pivot
-		right--;
-		//make sure left position shift over right(left == right, then left++)
-		while(left <= right){
-			//beacause the array is dynamically sorting, pivot has to be a fix value(rather than array[pivotPos], the value at that pos is changing)
-			if(array[left] < pivot){
-				left++;
-			}else if(array[right] >= pivot){
-				right--;
-			}else{
-				swap(array,left++,right--);
+	private int sortArray(int[] array, int left, int right){
+		int randomPivot = getRandomPivot(left, right);
+		int pivot = array[randomPivot];
+		int leftBound = left;
+		swap(array,right,randomPivot);
+		int rightBound = right - 1;
+		while(leftBound <= rightBound){
+			if(array[leftBound] < pivot){
+				leftBound++;
+			}else if(array[rightBound] >= pivot){
+				rightBound--;
+			}else {
+				swap(array, leftBound++, rightBound--);
 			}
 		}
-		//endpos is the arrayend,which is the pivot, the 'right' is the last position of right pointer
-		//swap left pointer with pivot (rather than right) is because the break point is when left > right and array[left] > array[pivot]
-		swap(array,left,endPos);
-		return left;
+		swap(array,leftBound,right);
+		return leftBound;
 	}
 
-	//Math.random() returns a number from zero to one.
-	private int randomPivot(int left, int right){
-		return left + (int)(Math.random()*(right - left + 1));
+	private int getRandomPivot(int left, int right){
+		return left + (int)Math.random()*(right - left + 1);
 	}
 	
 	private void swap(int[] array, int left, int right) {
